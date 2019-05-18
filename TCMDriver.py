c2009372207792eec8d3f01615e64eabac9791ed
Nahuel Aguilar <nahuelaguilar93@gmail.com>
2019-05-18 13:33:02 -0400

    Version correction. More explicit SPI config. First successful connection. Yay!

--- TCMDriver.py
+++ TCMDriver.py
@@ -1,11 +1,27 @@
 import spidev
 import time
 
-GET_DEVICE_INFO = [0x30, 0x01, 0x01, 0x0F]
+# Connection table
+# +------+----------+----------------------------------+-------------------------------------+----------------+
+# | Pin  |   Name   |             Remarks              |               Details               |   Connect to   |
+# +------+----------+----------------------------------+-------------------------------------+----------------+
+# |    1 | GND      | Supply ground                    |                                     | RPI-39         |
+# |    2 | /TC_EN   | TC enable                        |                                     | RPI-34         |
+# |    3 | VDDIN    | Power supply for digital part    | Op: 2.7 - 3.3 V | Abs: 0 - 3.6 V    | RPI-17         |
+# |    4 | VIN      | Power supply for analog part     | Op: 2.0 - 5.5 V | Abs: -0.3 - 6.0 V | RPI-02         |
+# |    5 | /TC_BUSY | Host interface busy output       |                                     | (disconnected) |
+# |    6 | TC_MISO  | Host interface data output       |                                     | RPI-21         |
+# |    7 | TC_MOSI  | Host interface data input        |                                     | RPI-19         |
+# |    8 | /TC_CS   | Host interface chip select input |                                     | RPI-24         |
+# |    9 | TC_SCK   | Host interface clock input       |                                     | RPI-23         |
+# |   10 | GND      | Supply ground                    |                                     | RPI-30         |
+# +------+----------+----------------------------------+-------------------------------------+----------------+
+
+GET_DEVICE_INFO = [0x30, 0x01, 0x01, 0x00]
 POLL_RESPONSE = [0x00, 0x00]
 
 class TCMConnection():
-    deviceInfo = 'MpicoSys TC-P74-230_v1.0'
+    DEVICE_INFO = 'MpicoSys TC-P74-230_v1.1'
     
     def __init__(self, bus=0, device=0):
         self.spi = spidev.SpiDev()
@@ -19,15 +35,29 @@ class TCMConnection():
         # This was verified as correct by testing the output signal using Analog Discorvery.
         self.spi.mode = 0b11
         # According to TCM datasheet: Bit order – MSB first
-        spi.lsbfirst = False
+        # According to library documentation, spi.lsbfirst might be read only,
+        # so just make sure it is false or we might need to invert data locally.
+        assert self.spi.lsbfirst == False
+        self.spi.cshigh = False
+        self.spi.bits_per_word = 8
+        
+        
+    def reverseBits(byte):
+        """ According to library documentation, spi.lsbfirst might be hardware fixed,
+            This method might be of help when changing to another master device. """
+        byte = ((byte & 0xF0) >> 4) | ((byte & 0x0F) << 4)
+        byte = ((byte & 0xCC) >> 2) | ((byte & 0x33) << 2)
+        byte = ((byte & 0xAA) >> 1) | ((byte & 0x55) << 1)
+        return byte
 
     def __del__(self):
         self.spi.close()
         
     def getDeviceInfo(self):
         self.spi.writebytes(GET_DEVICE_INFO)
-        time.sleep(0.001) # Wait until bussy is off.
-        return self.spi.readbytes(len(self.deviceInfo)+1+2)
+        # Wait until bussy is off. This should be replaced by a busy byte read. Abs minimum 28us (T_A+T_BUSY+T_NS)
+        time.sleep(0.001)
+        return self.spi.readbytes(len(self.DEVICE_INFO)+1+2)
     
     def verifyConnection(self):
         deviceInfoResponse = self.getDeviceInfo()
@@ -45,8 +75,10 @@ class TCMConnection():
             print("deviceInfo: " + deviceInfo)
             statusCode = deviceInfoResponse[strEnd+1:strEnd+3]
             print("statusCode: " + str(statusCode))
-            if (deviceInfo == self.deviceInfo and
-                statusCode == [0x90, 0x00]):
+            
+            if (deviceInfo == self.DEVICE_INFO and
+                statusCode[0] == 0x90 and
+                statusCode[1] == 0x00):
                 print("DeviceInfo is the expected.")
                 print("Status code is the expected.")
                 print("Connection Success!")
