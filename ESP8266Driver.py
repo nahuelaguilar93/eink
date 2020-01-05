5b9e74000101cf563c042d41447783dad199ceac
Nahuel Aguilar <nahuelaguilar93@gmail.com>
2020-01-05 13:38:52 -0500

    .

--- ESP8266Driver.py
+++ ESP8266Driver.py
@@ -1,3 +1,114 @@
 import serial
-ser= serial.Serial("/dev/ttyS0", baudrate=115200, timeout=3.0)
-ser.write([0xAA])
+import io
+import time
+import logging
+logging.basicConfig( format = '%(asctime)s [%(levelname)s] %(message)s',
+                     level  = logging.DEBUG )
+
+class ESP8265():
+    def __init__( self ):
+        self.serial = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=5.0 )
+        self._link = None
+
+    def _read( self, decodeLecture = True ):
+        i = 0
+        while True:
+            if self.serial.inWaiting() > 0:
+                break;
+            time.sleep( 0.001 )
+            logging.debug( 'waiting... {}'.format( i ) )
+            i += 1
+        lines = self.serial.readlines( )
+        if not decodeLecture:
+            logging.info('retrieving {} non decoded lines'.format(len(lines)))
+            return lines
+
+        decodedLines = []        
+        for line in lines:
+            try:
+                decodedLine = line.decode( 'utf-8' ).rstrip('\n').rstrip('\r')
+                logging.info( decodedLine )
+                decodedLines.append( decodedLine )
+            except:
+                logging.warning( 'fail to decode line: {}'.format( line ) )
+#                decodedLines.append( line )
+        return decodedLines 
+
+    def _blockingWrite( self, string, terminator = "\r\n", decodeLecture = True ):
+        self.serial.write( bytes( string + terminator, 'utf-8' ) )
+        lecture = self._read( decodeLecture = decodeLecture )
+        return lecture
+    
+    def _verifyNotBussy( self ):
+        return True
+
+    def __del__( self ):
+        self.serial.close()
+        
+    def reset( self ):
+        logging.info( 'Reseting Board' )
+        return self._blockingWrite( 'AT+RST' )
+
+    def factoryReset( self ):
+        logging.info( 'Applying Factory Reset to Board' )
+        return self._blockingWrite( 'AT+RESTORE' )
+
+    def setWifiModeToClient( self ):
+        logging.info( 'Setting WiFi Mode to client' )
+        return self._blockingWrite( 'AT+CWMODE_CUR=1' )
+
+    def wifiConnect( self, ssid, pwd ):
+        logging.info( 'Connecting to Access Point: {}'.format( ssid ) )
+        return self._blockingWrite( 'AT+CWJAP_CUR="{}","{}"'.format( ssid, pwd ) )
+        
+    def establishTCPConnection( self, link, port=80 ):
+        link       = link.rstrip( '/' )
+        self._link = link
+        logging.info( 'Establishing TCP Connection: {}'.format( link ) )
+        return self._blockingWrite( 'AT+CIPSTART="TCP","{}",{}'.format( link, port ) )
+
+    def getRequest( self, request ):
+        message = 'GET {}{} HTTP/1.0\r\n\r\n\r\n'.format( self._link, request )
+        logging.info( 'Sending request: {} | len {}'.format( message, len( message ) ) )
+        self._blockingWrite( 'AT+CIPSEND={}'.format( len( message ) ) )
+        return self._blockingWrite( message, terminator = '', decodeLecture = False )
+        
+def stripBytes(byteIn, stripStr):
+    encodedStrip = bytes(stripStr,'utf-8')
+    l = len(encodedStrip)
+    if byteIn[:l] == encodedStrip:
+        byteIn = byteIn[l:]
+    if byteIn[-l:] == encodedStrip:
+        byteIn = byteIn[:-l]
+    return byteIn
+
+def getConnection():
+    esp = ESP8265()
+    esp.reset()
+    #esp.factoryReset()
+    esp.setWifiModeToClient()
+    esp.wifiConnect( 'PochiNet 0.5', 'prohibidofumar247' )
+    #esp.wifiConnect( 'Nahuel Android', '66666666' )
+    return esp
+
+def getTimeToSleep(esp):
+    esp.establishTCPConnection( 'http://750ba94f.ngrok.io' )
+    response = esp.getRequest( '/aula115/time_till_wake_up' )
+    return int(response[-1].decode('utf-8').strip('\r\n').strip('CLOSED'))
+
+def getImageData(esp):
+    esp.establishTCPConnection( 'http://750ba94f.ngrok.io' )
+    b = esp.getRequest( '/aula115/digested_image' )
+    toStrip = ['\r\n','CLOSED','+IPD,1460:','+IPD,1437:']
+    for stripIt in toStrip:
+        b = [stripBytes(x, stripIt) for x in b]
+    q = bytes('','utf-8')    
+    for t in range(11, len(b)):
+        q += b[t]
+    return q
+
+def demo():
+    esp = getConnection()
+    q = getImageData(esp)
+    from TCMDriver import TCMConnection
+    TCMConnection().writeFullScreen(q[:48000])
--- drawer.py
+++ drawer.py
@@ -1,24 +1,44 @@
 from TCMDriver import TCMConnection
-from PIL import Image, ImageOps
-from utils import bitsToFormatType4bytes
-import requests
+from ESP8266Driver import ESP8265, getImageData, getTimeToSleep, getConnection
+from time import sleep
+import logging
+logging.basicConfig( format = '%(asctime)s [%(levelname)s] %(message)s',
+                     level  = logging.DEBUG )
 
-link = 'https://a0e41fa6.ngrok.io/aula115/'
-print('downloading img')
-img = requests.get(link)
-# assert response is OK
-if img.status_code == 200:
-    print('img downloaded')
-    f = open('/home/pi/Documents/eink/aula115.png', 'wb+')
-    f.write(img.content)
-    f.close()
-    print('img saved')
+NGROK_CODE = "750ba94f"
 
-conn = TCMConnection()
-v1 = Image.open('/home/pi/Documents/eink/aula115.png')
-v2 = ImageOps.invert(v1.convert('RGB'))
-v3 = v2.convert('1')
-v4 = v3.transpose(Image.ROTATE_270)
-d = list(v4.getdata())
-byteArray = bitsToFormatType4bytes(d)
-conn.writeFullScreen(tuple(byteArray))
+def retrieveData( link ):
+    import requests
+    response = requests.get(link)
+    if response.status_code != 200:
+        raise Exception('Error retrieving data.')
+    return response.content
+
+def retrieveImgData2():
+    DIMG_LINK = 'https://{}.ngrok.io/aula115/digested_image'.format(NGROK_CODE)
+    return list(retrieveData(DIMG_LINK))
+
+def retrieveSleepTime():
+    esp = ESP8265()
+    esp.reset()
+    #esp.factoryReset()
+    esp.setWifiModeToClient()
+    esp.wifiConnect( 'PochiNet 0.5', 'prohibidofumar247' )
+    #esp.wifiConnect( 'Nahuel Android', '66666666' )
+    esp.establishTCPConnection( 'http://750ba94f.ngrok.io' )
+    wawa = esp.getRequest( '/aula115/time_till_wake_up' )
+    return int(wawa[-1].strip('CLOSED'))
+
+for iteration in range(3):
+    esp = getConnection()
+    
+    logging.info('retrieving img...')
+#    imgData = retrieveImgData()
+    imgData = getImageData(esp)
+    logging.info('img downloaded.')    
+
+    TCMConnection().writeFullScreen(imgData)
+    
+    sleepTime = getTimeToSleep(esp)
+    logging.info('sleeping for {} seconds...'.format(sleepTime))
+    sleep(sleepTime)
